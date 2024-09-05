c8ab713f25f785207b15981369d17e70eb64871b
yang <Yhmecnu@163.com>
2024-09-05 16:30:33 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .run import *
+from .utils import DBHelper
+from .processer import Processer
+
+__all__ = ["DBHelper", "Processer"]  # 公开的对象名称
--- run.py
+++ run.py
@@ -17,82 +17,112 @@ FUNC_MAP = {
 }
 
 
-def run_pre(
-    func_name: Literal["index_tick", "mayjor_tick", "secondery_tick", "option_1m"]
-):
-    try:
-        func = FUNC_MAP[func_name]
-        logger.add(f"logs/{func_name}.log", level="WARNING", rotation="10 MB")
-        results = func()
-        logger.success(f"[{func_name}] Success:: {results}")
-    except Exception as e:
-        logger.error(f"[{e.__class__.__name__}] Error:: {e}")
-    finally:
-        pass
+class DataRunner:
 
+    @staticmethod
+    def run_pre(
+        func_name: Literal["index_tick", "mayjor_tick", "secondery_tick", "option_1m"]
+    ):
+        """
+        处理主连、次主力连、指数合约的tick数据或者期权的1分钟数据。因为这些数据可以直接处理, 不需要依赖其他数据.
+        @parmeter
+        :param func_name: 如`index_tick`代表处理指数tick, `mayjor_tick`代表主力合约tick,\
+                        `secondery_tick`代表处理次主力合约tick, `option_1m`代表处理期权的1分钟数据
+        """
+        try:
+            func = FUNC_MAP[func_name]
+            logger.add(f"logs/{func_name}.log", level="WARNING", rotation="10 MB")
+            results = func()
+            logger.success(f"[{func_name}] Success:: {results}")
+        except Exception as e:
+            logger.error(f"[{e.__class__.__name__}] Error:: {e}")
+        finally:
+            pass
 
-def run_after(
-    func_name: Literal["future_1m", "future_1d", "option_1d"],
-    kind: Literal["index", "mayjor", "secondery"],
-):
-    try:
-        func = FUNC_MAP[func_name]
-        logger.add(f"logs/{func_name}/{kind}.log", level="WARNING", rotation="10 MB")
-        match func_name.split("_")[0]:
-            case "future":
-                results = func(kind)
-            case "option":
-                results = func()
-            case _:
-                raise ValueError(
-                    "Invalid function name. It should be 'future_1m' or 'future_1d' or 'option_1d'."
-                )
-        logger.success(f"[{kind}] Success:: {results}")
-    except Exception as e:
-        logger.error(f"[{e.__class__.__name__}] Error:: {e}")
-    finally:
-        pass
+    @staticmethod
+    def run_after(
+        func_name: Literal["future_1m", "future_1d", "option_1d"],
+        kind: Literal["index", "mayjor", "secondery", "option"],
+    ):
+        """
+        处理1m数据需要现有tick数据, 处理1d数据需要先有1m数据. 因此运行该函数之前, 需确保有相应的数据.
+        @parmeter
+        :param func_name: `future_1m`代表处理期货的1分钟线数据, 其他类推.
+        :param kind: 期货数据的类别, 如`index`代表指数, `mayjor`代表主力合约, `secondery`代表次主力合约.\
+                    如果第一个参数是`option_1d`, 则此参数无效, 仅代表日志名
+        """
+        try:
+            func = FUNC_MAP[func_name]
+            logger.add(
+                f"logs/{func_name}/{kind}.log", level="WARNING", rotation="10 MB"
+            )
+            match func_name.split("_")[0]:
+                case "future":
+                    results = func(kind)
+                case "option":
+                    results = func()
+                case _:
+                    raise ValueError(
+                        "Invalid function name. It should be 'future_1m' or 'future_1d' or 'option_1d'."
+                    )
+            logger.success(f"[{kind}] Success:: {results}")
+        except Exception as e:
+            logger.error(f"[{e.__class__.__name__}] Error:: {e}")
+        finally:
+            pass
 
+    @staticmethod
+    def run_all_pre():
+        """
+        处理主连、次主力连、指数合约的tick数据以及期权的1分钟数据。因为这些数据可以直接处理, 不需要依赖其他数据.
+        """
+        num_processes = multiprocessing.cpu_count()
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            pool.apply_async(DataRunner.run_pre, args=("index_tick",))
+            pool.apply_async(DataRunner.run_pre, args=("mayjor_tick",))
+            pool.apply_async(DataRunner.run_pre, args=("secondery_tick",))
+            pool.apply_async(DataRunner.run_pre, args=("option_1m",))
+            pool.close()
+            pool.join()
 
-def run_all_pre():
-    num_processes = multiprocessing.cpu_count()
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.apply_async(run_pre, args=("index_tick",))
-        pool.apply_async(run_pre, args=("mayjor_tick",))
-        pool.apply_async(run_pre, args=("secondery_tick",))
-        pool.apply_async(run_pre, args=("option_1m",))
-        pool.close()
-        pool.join()
+    @staticmethod
+    def run_all_1m():
+        """
+        处理主连、次主力连、指数合约的1m数据. 需要确保有相应的tick数据.
+        """
+        num_processes = multiprocessing.cpu_count()
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            pool.apply_async(DataRunner.run_after, args=("future_1m", "index"))
+            pool.apply_async(DataRunner.run_after, args=("future_1m", "mayjor"))
+            pool.apply_async(DataRunner.run_after, args=("future_1m", "secondery"))
+            pool.close()
+            pool.join()
 
+    @staticmethod
+    def run_all_1d():
+        """
+        处理主连、次主力连、指数合约以及期权的1d数据. 需要确保有相应的1m数据.
+        """
+        num_processes = multiprocessing.cpu_count()
+        with multiprocessing.Pool(processes=num_processes) as pool:
+            pool.apply_async(DataRunner.run_after, args=("futrue_1d", "index"))
+            pool.apply_async(DataRunner.run_after, args=("future_1d", "mayjor"))
+            pool.apply_async(DataRunner.run_after, args=("future_1d", "secondery"))
+            pool.apply_async(DataRunner.run_after, args=("option_1d", "option"))
+            pool.close()
+            pool.join()
 
-def run_all_1m():
-    num_processes = multiprocessing.cpu_count()
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.apply_async(run_after, args=("future_1m", "index"))
-        pool.apply_async(run_after, args=("future_1m", "mayjor"))
-        pool.apply_async(run_after, args=("future_1m", "secondery"))
-        pool.close()
-        pool.join()
-
-
-def run_all_1d():
-    num_processes = multiprocessing.cpu_count()
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.apply_async(run_after, args=("futrue_1d", "index"))
-        pool.apply_async(run_after, args=("future_1d", "mayjor"))
-        pool.apply_async(run_after, args=("future_1d", "secondery"))
-        pool.apply_async(run_after, args=("option_1d", "option"))
-        pool.close()
-        pool.join()
-
-
-def run_data_supplement():
-    logger.remove()
-    logger.add(sys.stderr, level="SUCCESS")
-    run_all_pre()
-    run_all_1m()
-    run_all_1d()
+    @staticmethod
+    def run_data_supplement():
+        """
+        补充主连、次主力连、指数合约以及期权的tick、1m、1d数据. 主函数接口
+        """
+        logger.remove()
+        logger.add(sys.stderr, level="SUCCESS")
+        DataRunner.run_all_pre()
+        DataRunner.run_all_1m()
+        DataRunner.run_all_1d()
 
 
 if __name__ == "__main__":
-    run_data_supplement()
+    DataRunner.run_data_supplement()
